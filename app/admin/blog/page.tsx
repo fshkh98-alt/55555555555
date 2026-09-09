d62f53a075f36954b640bb4998da3546176f959a
fshkh98-alt <fshkh98@gmail.com>
2026-09-09 22:52:31 +0300

    Update page.tsx

--- app/admin/blog/page.tsx
+++ app/admin/blog/page.tsx
@@ -1,4 +1,4 @@
 import { ResourcePage } from "../resource-page";
 import { PostsManager } from "../../../components/resource-manager";
 export const dynamic="force-dynamic";
-export default function Page(){return <ResourcePage resource="post" active="Blog">{items:any[]=><PostsManager items={items}/>}</ResourcePage>}
+export default function Page(){return <ResourcePage resource="post" active="Blog">{(items:any[])=><PostsManager items={items}/>}</ResourcePage>}
